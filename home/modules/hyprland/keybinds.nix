5fce03042eb0e8e5c06b06257ecd1a5f30e879b7
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-20 21:33:38 +0100

    Added fuzzel and keybinds configurations for improved window management in Hyprland

---
+++ home/modules/hyprland/keybinds.nix
@@ -0,0 +1,59 @@
+{ config, pkgs, ... }:
+
+{
+  wayland.windowManager.hyprland.settings = {
+    # Variables
+    "$mainMod" = "SUPER";
+    "$terminal" = "kitty";
+    "$fileManager" = "nautilus";
+    "$menu" = "fuzzel";
+
+    # See https://wiki.hypr.land/Configuring/Binds/
+    bind = [
+      # Application shortcuts
+      "$mainMod, Q, exec, $terminal"
+      "$mainMod, C, killactive,"
+      "$mainMod, M, exit,"
+      "$mainMod, E, exec, $fileManager"
+      "$mainMod, V, togglefloating,"
+      "$mainMod, R, exec, $menu"
+      "$mainMod, P, pseudo," # dwindle
+      "$mainMod, J, togglesplit," # dwindle
+
+      # Move focus with mainMod + arrow keys
+      "$mainMod, left, movefocus, l"
+      "$mainMod, right, movefocus, r"
+      "$mainMod, up, movefocus, u"
+      "$mainMod, down, movefocus, d"
+
+      # Switch workspaces with mainMod + [0-9]
+      "$mainMod, 1, workspace, 1"
+      "$mainMod, 2, workspace, 2"
+      "$mainMod, 3, workspace, 3"
+      "$mainMod, 4, workspace, 4"
+      "$mainMod, 5, workspace, 5"
+      "$mainMod, 6, workspace, 6"
+      "$mainMod, 7, workspace, 7"
+      "$mainMod, 8, workspace, 8"
+      "$mainMod, 9, workspace, 9"
+      "$mainMod, 0, workspace, 10"
+
+      # Move active window to a workspace with mainMod + SHIFT + [0-9]
+      "$mainMod SHIFT, 1, movetoworkspace, 1"
+      "$mainMod SHIFT, 2, movetoworkspace, 2"
+      "$mainMod SHIFT, 3, movetoworkspace, 3"
+      "$mainMod SHIFT, 4, movetoworkspace, 4"
+      "$mainMod SHIFT, 5, movetoworkspace, 5"
+      "$mainMod SHIFT, 6, movetoworkspace, 6"
+      "$mainMod SHIFT, 7, movetoworkspace, 7"
+      "$mainMod SHIFT, 8, movetoworkspace, 8"
+      "$mainMod SHIFT, 9, movetoworkspace, 9"
+      "$mainMod SHIFT, 0, movetoworkspace, 10"
+    ];
+
+    # SUPER key alone opens/closes fuzzel (bindr = bind on key release)
+    bindr = [
+      "SUPER, Super_L, exec, pkill fuzzel || fuzzel"
+    ];
+  };
+}
