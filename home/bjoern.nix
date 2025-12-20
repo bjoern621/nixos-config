ef411dcf653d29be615389ca30835cf6c73db0e2
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-20 19:36:07 +0100

    Removed unused Hyprland configuration and cleaned up user packages.

--- home/bjoern.nix
+++ home/bjoern.nix
@@ -14,37 +14,7 @@
   home.packages = with pkgs; [
     google-chrome
     firefox
-    unzip
-    htop
-    neofetch
   ];
-
-  # Git configuration
-  programs.git = {
-    enable = true;
-    userName = "Björn";
-    userEmail = ""; # Set email address
-  };
-
-  # Terminal
-  programs.kitty = {
-    enable = true;
-    settings = {
-      font_size = 12;
-      background_opacity = "0.9";
-      confirm_os_window_close = 0;
-    };
-  };
-
-  # Shell
-  programs.bash = {
-    enable = true;
-    shellAliases = {
-      ll = "ls -la";
-      rebuild = "sudo nixos-rebuild switch --flake /etc/nixos#nixos";
-      update = "nix flake update /etc/nixos";
-    };
-  };
 
   home.stateVersion = "25.11";
 
