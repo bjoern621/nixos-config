f05c61deb4c73b0ee75953daf054ce1b9fd1038c
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-20 19:38:14 +0100

    Removed unused Hyprland configuration and system packages for a cleaner setup.

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,38 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  # Enable Hyprland
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
   };
 
-  # Hint Electron apps to use Wayland
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-    WLR_NO_HARDWARE_CURSORS = "1";
-  };
-
-  # XDG Portal for Hyprland
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
-  };
-
-  # System packages required for Hyprland
-  environment.systemPackages = with pkgs; [
-    kitty
-    waybar
-    wofi
-    dunst
-    swww
-    grim
-    slurp
-    wl-clipboard
-    brightnessctl
-    networkmanagerapplet
-  ];
-
-  # Enable polkit for authentication dialogs
-  security.polkit.enable = true;
+  # Hint electron apps to use wayland
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
