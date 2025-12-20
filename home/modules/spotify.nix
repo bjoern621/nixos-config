805269b957fa7f0d45385f9fcb768acbc572c856
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-20 19:36:46 +0100

    Removed unused desktop entry configurations for Discord and Spotify.

--- home/modules/spotify.nix
+++ home/modules/spotify.nix
@@ -1,19 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  # Spotify (user-level installation)
   home.packages = with pkgs; [
     spotify
   ];
-
-  # Spotify desktop entry modifications (optional)
-  xdg.desktopEntries.spotify = {
-    name = "Spotify";
-    genericName = "Music Player";
-    exec = "spotify %U";
-    terminal = false;
-    icon = "spotify-client";
-    categories = [ "Audio" "Music" "Player" "AudioVideo" ];
-    mimeType = [ "x-scheme-handler/spotify" ];
-  };
 }
