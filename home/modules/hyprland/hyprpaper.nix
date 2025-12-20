97828c2ede33eb02744991c18480c5629a6f11e6
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-20 20:51:13 +0100

    Updated wallpaper configuration to use a variable for improved maintainability

--- home/modules/hyprland/hyprpaper.nix
+++ home/modules/hyprland/hyprpaper.nix
@@ -1,5 +1,10 @@
 { config, pkgs, ... }:
 
+let
+  # Nix path to wallpaper file, copied to /nix/store at build time.
+  # Use ${wallpaper} to interpolate the absolute store path into configs.
+  wallpaper = ../../wallpapers/Ashes.jpg;
+in
 {
   home.packages = with pkgs; [
     hyprpaper
@@ -8,9 +13,8 @@
   # Create config file hyprpaper.conf
   # XDG = Cross-Desktop Group (https://www.freedesktop.org/wiki/)
   # specification that standardizes where apps should store files on Linux
-  xdg.configFile."hypr/hyprpaper.conf".text = 
-  ''
-    preload = ../../wallpapers/Ashes.jpg
-    wallpaper = ,../../wallpapers/Ashes.jpg
+  xdg.configFile."hypr/hyprpaper.conf".text = ''
+    preload = ${wallpaper}
+    wallpaper = ,${wallpaper}
   '';
 }
