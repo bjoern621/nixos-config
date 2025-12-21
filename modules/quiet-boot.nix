fce30eacdffb51f330031893c4b75beda7f9a886
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-21 13:13:39 +0100

    chagned boot

--- modules/quiet-boot.nix
+++ modules/quiet-boot.nix
@@ -1,7 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  boot.kernelParams {
+  boot.kernelParams = [
     "quiet"
-  };
+  ];
+
+  # Instantly select latest NixOS profile
+  boot.loader.timeout = 0;
+  boot.loader.systemd-boot.default = "auto";
 }
