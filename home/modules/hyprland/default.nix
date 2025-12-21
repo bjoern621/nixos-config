bd9318d2c048ff67c1fdfeb15c390e8b30287d7f
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-21 16:26:06 +0100

    waybar

--- home/modules/hyprland/default.nix
+++ home/modules/hyprland/default.nix
@@ -22,7 +22,7 @@
       # - HYPRCURSOR_* is Hyprland's cursor backend (used by Hyprland-native cursor handling).
       # Setting both keeps cursor theme consistent across Wayland-native and XWayland apps.
       env = [
-        "XCURSOR_THEME,Bibata-Modern-Ice"
+        "XCURSOR_THEME,Bibata-Modern-Ice" # https://www.gnome-look.org/p/1197198
         "XCURSOR_SIZE,24"
         "HYPRCURSOR_THEME,Bibata-Modern-Ice"
         "HYPRCURSOR_SIZE,24"
