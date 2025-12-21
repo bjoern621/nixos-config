3e091dc75665e08f4c75b2d5a70c7937933445d9
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-21 11:50:57 +0100

    try to fix audio

--- modules/pipewire.nix
+++ modules/pipewire.nix
@@ -1,11 +1,23 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  hardware.firmware = [
+    pkgs.sof-firmware
+  ];
+
+  environment.systemPackages = with pkgs; [
+    alsa-utils
+    easyeffects
+    pavucontrol
+    pwvucontrol
+  ];
+
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    wireplumber.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
