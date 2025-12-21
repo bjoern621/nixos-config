684ecdb646632a216dc14fc878092ec17ff71791
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-21 13:44:06 +0100

    added some apps

--- home/modules/hyprland/default.nix
+++ home/modules/hyprland/default.nix
@@ -34,7 +34,7 @@
         
         touchpad = {
           natural_scroll = true; # true: Swipe down -> content moves down
-          scroll_factor = 0.1;
+          scroll_factor = 0.2;
         };
       };
 
