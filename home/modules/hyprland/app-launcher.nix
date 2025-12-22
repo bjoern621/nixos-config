a86299edb492093229203b9aac34977cdcb6d283
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-22 22:18:05 +0100

    Update app-launcher.nix

--- home/modules/hyprland/app-launcher.nix
+++ home/modules/hyprland/app-launcher.nix
@@ -4,4 +4,9 @@
   home.packages = with pkgs; [
     rofi
   ];
+
+  # SUPER key alone opens/closes fuzzel (bindr = bind on key release)
+  wayland.windowManager.hyprland.settings.bindr = [
+    "SUPER, Super_L, exec, pkill rofi || rofi -show drun -show-icons"
+  ];
 }
