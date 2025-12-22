d8aed25769e4536760704bf5c99e750732019fe5
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-22 20:09:42 +0100

    change vscode start options

--- home/modules/vscode.nix
+++ home/modules/vscode.nix
@@ -2,4 +2,18 @@
 
 {
   programs.vscode.enable = true;
+
+  xdg.desktopEntries."code" = {
+    name = "Visual Studio Code";
+    genericName = "Text Editor";
+    comment = "Code Editing. Redefined.";
+    exec = "code --password-store="gnome-libsecret"  --touch-events %F";
+    terminal = false;
+    icon = "vscode";
+    categories = [
+      "Development"
+      "IDE"
+    ];
+    type = "Application";
+  };
 }
