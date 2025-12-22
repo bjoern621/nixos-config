b604c5d65d26254d1a6b96bd2490d1e71c172a61
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-22 17:01:10 +0100

    Enable GNOME Keyring and Seahorse services
    Added configuration to enable GNOME Keyring and Seahorse.

--- modules/keyring.nix
+++ modules/keyring.nix
@@ -1,8 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    gnome-keyring
-    seahorse
-  ];
+  # See also: https://search.nixos.org/options?channel=25.11&query=keyring
+
+  # Enable system secrets manager.
+  # Allows apps to store credentials (like VSCode Settings Sync Account) securely.
+  # Uses GNOME Keyring (other alternatives are Kwallet).
+  services.gnome.gnome-keyring.enable = true; 
+
+  # Enable GUI for showing stored secrets.
+  programs.seahorse.enable = true; 
 }
