76b202f749901e0101151c94e81e1878b867a903
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-22 20:20:46 +0100

    Update vscode.nix

--- home/modules/vscode.nix
+++ home/modules/vscode.nix
@@ -7,7 +7,7 @@
     name = "Visual Studio Code";
     genericName = "Text Editor";
     comment = "Code Editing. Redefined.";
-    exec = "code --password-store="gnome-libsecret"  --touch-events %F";
+    exec = "code --password-store='gnome-libsecret' --touch-events %F";
     terminal = false;
     icon = "vscode";
     categories = [
