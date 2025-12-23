23ccac67cae57d1dca3790b58e8b6b4f4fec1a8b
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-23 19:36:52 +0100

    Update hyprland.nix

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -4,6 +4,7 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
   };
 
   # Hint electron apps to use wayland
