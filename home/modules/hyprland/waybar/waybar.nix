29b2f683f7674c0037f1f7a9e3b119b43c33aca4
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-25 21:33:12 +0100

    update waybar

--- home/modules/hyprland/waybar/waybar.nix
+++ home/modules/hyprland/waybar/waybar.nix
@@ -7,45 +7,8 @@ in
   programs.waybar = {
     enable = true;
     style = builtins.readFile ./waybar.css;
-    settings = {
-      mainBar = {
-        layer = "top";
-        position = "top";
-
-        modules-left = [ "hyprland/workspaces" ];
-        modules-center = [ "clock" ];
-        modules-right = [ "tray" "battery" "pulseaudio" "network" "custom/powermenu" ];
-
-        "clock" = {
-          format = " {:L%I:%M %p}";
-        };
-
-        "battery" = {
-          format = "{capacity}% {icon}";
-          format-icons = ["" "" "" "" ""];
-        };
-        
-        "pulseaudio" = {
-          format = "{volume}% {icon}";
-          format-icons = {
-            default = ["" "" ""];
-          };
-          on-click = "pavucontrol";
-        };
-
-        "network" = {
-          format-wifi = " ";
-          format-ethernet = "󰈀 ";
-          format-disconnected = "󰖪";
-          on-click = "nm-connection-editor";
-        };
-
-        "custom/powermenu" = {
-          exec = "echo '⏻'";
-          format = "{output}";
-          on-click = "${powerMenu}/bin/power-menu";
-        };
-      };
+    settings = builtins.fromJSON (builtins.readFile ./waybar.jsonc) // {
+      mainBar."custom/powermenu"."on-click" = "${powerMenu}/bin/power-menu";
     };
   };
 
