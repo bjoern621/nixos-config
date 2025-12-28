9bcf6dc7f98c9770d490163195715ea633669951
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-28 02:05:26 +0100

    Corrected active_border assignment in Hyprland configuration

--- home/modules/hyprland/default.nix
+++ home/modules/hyprland/default.nix
@@ -27,7 +27,8 @@
         border_size = 1;
         gaps_out = "0,12,12,12";
         gaps_in = 4;
-        col.active_border = "rgba(255,255,255,0.1)";
+        active_border = "rgba(255,255,255,0.1)";
+        resize_on_border = true;
       };
 
       # Cursor variables:
