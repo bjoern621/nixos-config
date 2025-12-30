3c79b3f280d09c17228b3c6c4430b381f0de20ac
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-31 00:30:12 +0100

    tmp disable monitors

--- modules/monitors.nix
+++ modules/monitors.nix
@@ -2,8 +2,8 @@
 
 {
     # https://wiki.hypr.land/Configuring/Monitors/
-    monitor = eDP-1, 2944x1840@90, 0x0, 2, bitdepth, 10, cm, wide
-    monitor = desc:LG Electronics LG ULTRAGEAR 308MAPN9YD64, 2560x1440@144, 1472x0, 1 # Left side LG monitor
+    monitor = eDP-1, 2944x1840@90, 0x0, 2, bitdepth, 10, cm, wide, disable
+    monitor = desc:LG Electronics LG ULTRAGEAR 308MAPN9YD64, 2560x1440@144, 1472x0, 1, disable # Left side LG monitor
     monitor = desc:LG Electronics LG ULTRAGEAR 308MAVD9YD63, 2560x1440@144, 4032x0, 1 # Right side LG monitor
     monitor = , preferred, auto, 1 # Fallback
 }
