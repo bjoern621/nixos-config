a9427521b7fbad80b0a150921d0e862bb11dd36c
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-30 15:09:15 +0100

    Added uwsm environment configuration for improved session management

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -11,7 +11,11 @@
 
     # https://wiki.hypr.land/Useful-Utilities/Systemd-start/#uwsm
     withUWSM = true;
-};
+
+  };
+  
+  # https://wiki.hypr.land/Nix/Hyprland-on-Home-Manager/#nixos-uwsm
+  xdg.configFile."uwsm/env".source = "${config.home.sessionVariablesPackage}/etc/profile.d/hm-session-vars.sh"; 
 
   # Hint electron apps to use wayland
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
