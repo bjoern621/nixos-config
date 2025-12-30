2053390959b4591b14d03f4dddc05504f8c2c7ff
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-30 18:33:22 +0100

    Updated monitor configuration for eDP-1 to enable wide aspect ratio support

--- modules/monitors.nix
+++ modules/monitors.nix
@@ -2,7 +2,7 @@
 
 {
     # https://wiki.hypr.land/Configuring/Monitors/
-    monitor = eDP-1, 2944x1840@90, 0x0, 2, bitdepth, 10 # VRR enabled
+    monitor = eDP-1, 2944x1840@90, 0x0, 2, bitdepth, 10, cm, wide
     monitor = desc:LG Electronics LG ULTRAGEAR 308MAPN9YD64, 2560x1440@144, 1472x0, 1 # Left side LG monitor
     monitor = desc:LG Electronics LG ULTRAGEAR 308MAVD9YD63, 2560x1440@144, 4032x0, 1 # Right side LG monitor
     monitor = , preferred, auto, 1 # Fallback
