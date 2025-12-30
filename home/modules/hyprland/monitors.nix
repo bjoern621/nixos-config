7ddce340ffcb432ed14684e0533f2ef5791444ac
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-31 00:55:42 +0100

    Updated monitor configuration for improved display settings

--- home/modules/hyprland/monitors.nix
+++ home/modules/hyprland/monitors.nix
@@ -4,12 +4,11 @@
   wayland.windowManager.hyprland.settings = {
     # https://wiki.hypr.land/Configuring/Monitors/
     monitor = [
-      "eDP-1,disable"
-      "desc:LG Electronics LG ULTRAGEAR 308MAPN9YD64,2560x1440@144,1472x0,1,disable"
+      "monitor = eDP-1, 2944x1840@90, 0x0, 2, bitdepth, 10, cm, wide"
+      "desc:LG Electronics LG ULTRAGEAR 308MAPN9YD64,disable,2560x1440@144,1472x0,1"
       "desc:LG Electronics LG ULTRAGEAR 308MAVD9YD63,2560x1440@144,4032x0,1"
       ",preferred,auto,1"
     ];
   };
 }
 
-#monitor = eDP-1, 2944x1840@90, 0x0, 2, bitdepth, 10, cm, wide, disable
