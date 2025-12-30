26c710731394c2cde13d7a1bff8bd16b05604530
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-30 17:39:28 +0100

    Removed unnecessary parameters from monitor configurations for clarity

--- modules/monitors.nix
+++ modules/monitors.nix
@@ -2,8 +2,8 @@
 
 {
     # https://wiki.hypr.land/Configuring/Monitors/
-    monitor = eDP-1, 2944x1840@90, 0x0, 2, bitdepth, 10, vrr, 1 # VRR enabled
-    monitor = desc:LG Electronics LG ULTRAGEAR 308MAVD9YD63, 2560x1440@144, 1472x0, 1, bitdepth, 10 # Right side LG monitor (using right side USB C)
+    monitor = eDP-1, 2944x1840@90, 0x0, 2, bitdepth, 10 # VRR enabled
+    monitor = desc:LG Electronics LG ULTRAGEAR 308MAVD9YD63, 2560x1440@144, 1472x0, 1 # Right side LG monitor (using right side USB C)
     monitor = , preferred, auto, 1 # Fallback
 
 
