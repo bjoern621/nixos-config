86cba8ab34647d23540dbe4325002364e79d6dfe
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-31 16:24:50 +0100

    missing quote

--- home/modules/hyprland/preferred-workspaces.nix
+++ home/modules/hyprland/preferred-workspaces.nix
@@ -12,7 +12,7 @@
       "match:class code, workspace 2"
       "match:class spotify, workspace 3"
       "match:class discord, workspace 3"
-      "match:class mpv, float on
+      "match:class mpv, float on"
     ];
 
     workspace = [
