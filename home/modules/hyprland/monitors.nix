2f58ad1eb377bed038ccd825f7738e2b4389905f
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-31 01:28:38 +0100

    corrected color gamut

--- home/modules/hyprland/monitors.nix
+++ home/modules/hyprland/monitors.nix
@@ -4,7 +4,7 @@
   wayland.windowManager.hyprland.settings = {
     # https://wiki.hypr.land/Configuring/Monitors/
     monitor = [
-      "eDP-1, 2944x1840@90, 0x0, 2, cm, wide"
+      "eDP-1, 2944x1840@90, 0x0, 2, bitdepth, 10, cm, dcip3"
       "desc:LG Electronics LG ULTRAGEAR 308MAPN9YD64,disable,2560x1440@144,1472x0,1"
       "desc:LG Electronics LG ULTRAGEAR 308MAVD9YD63,2560x1440@144,4032x0,1"
       ",preferred,auto,1"
