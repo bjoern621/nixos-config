877b3abf6dd6a08720af510b704ef7018c6b5b29
Björn <41452212+bjoern621@users.noreply.github.com>
2026-01-01 11:53:17 +0100

    readd cachix

--- flake.nix
+++ flake.nix
@@ -23,15 +23,14 @@
         modules = [
           ./hosts/default/configuration.nix
 
-          # TODO fix "unable to download"
-          # {
-          #   # https://wiki.hypr.land/Nix/Cachix/
-          #   nix.settings = {
-          #     substituters = ["https://hyprland.cachix.org"];
-          #     trusted-substituters = ["https://hyprland.cachix.org"];
-          #     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-          #   };
-          # }
+          {
+            # https://wiki.hypr.land/Nix/Cachix/
+            nix.settings = {
+              extra-substituters = ["https://hyprland.cachix.org"];
+              extra-trusted-substituters = ["https://hyprland.cachix.org"];
+              extra-trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+            };
+          }
 
           home-manager.nixosModules.home-manager
           {
