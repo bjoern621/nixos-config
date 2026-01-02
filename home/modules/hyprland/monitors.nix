70ddef4cfa2c53545984a7ab00578d60a1007767
Björn <41452212+bjoern621@users.noreply.github.com>
2026-01-02 13:20:21 +0100

    changed monitors

--- home/modules/hyprland/monitors.nix
+++ home/modules/hyprland/monitors.nix
@@ -5,8 +5,8 @@
     # https://wiki.hypr.land/Configuring/Monitors/
     monitor = [
       "eDP-1, 2944x1840@90, 0x0, 2"
-      "desc:LG Electronics LG ULTRAGEAR 308MAPN9YD64,2560x1440@120,auto,1"
-      "desc:LG Electronics LG ULTRAGEAR 308MAVD9YD63,2560x1440@120,auto,1"
+      "desc:LG Electronics LG ULTRAGEAR 308MAPN9YD64,2560x1440@100,auto,1"
+      "desc:LG Electronics LG ULTRAGEAR 308MAVD9YD63,2560x1440@100,auto,1"
       ",preferred,auto,1"
     ];
   };
