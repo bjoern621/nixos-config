07c5871ce060d3b0cbaa0995baf6c2bdf3727f7c
Björn <41452212+bjoern621@users.noreply.github.com>
2026-01-10 20:16:05 +0100

    ags bar

--- home/modules/ags/Bar/Clock.tsx
+++ home/modules/ags/Bar/Clock.tsx
@@ -2,9 +2,11 @@ import GLib from "gi://GLib";
 import Gtk from "gi://Gtk?version=4.0";
 import { createPoll } from "ags/time";
 
-export default function Clock({ format = "%H:%M" }) {
+export default function Clock() {
     const time = createPoll("", 1000, () => {
-        return GLib.DateTime.new_now_local().format(format)!;
+        return GLib.DateTime.new_now_local().format(
+            " %a, %e %b \u00B7 %H:%M"
+        )!;
     });
 
     return (
