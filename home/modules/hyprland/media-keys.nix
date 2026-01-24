a6b948897529fddfc6b8b4dd53e4958fd4998712
Björn <41452212+bjoern621@users.noreply.github.com>
2026-01-24 17:33:20 +0100

    Removed unused media key bindings for cleaner configuration in Hyprland.

--- home/modules/hyprland/media-keys.nix
+++ home/modules/hyprland/media-keys.nix
@@ -11,12 +11,6 @@
     bindel = [
       ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
       ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-      ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-      ", XF86AudioNext, exec, playerctl next"
-      ", XF86AudioPrev, exec, playerctl previous"
-      ", XF86AudioPlay, exec, playerctl play-pause"
-      ", XF86MonBrightnessUp, exec, brightnessctl set +10%"
-      ", XF86MonBrightnessDown, exec, brightnessctl set 10%-"
     ];
 
     # l -> do stuff even when locked
@@ -25,8 +19,6 @@
       ", XF86AudioNext, exec, playerctl next"
       ", XF86AudioPrev, exec, playerctl previous"
       ", XF86AudioPlay, exec, playerctl play-pause"
-      ", XF86MonBrightnessUp, exec, brightnessctl set +10%"
-      ", XF86MonBrightnessDown, exec, brightnessctl set 10%-"
     ];
   };
 }
