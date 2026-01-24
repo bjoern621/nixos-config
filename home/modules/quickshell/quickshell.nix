093081b477de173fa2b55449c6d4ece93ce8e90f
Björn <41452212+bjoern621@users.noreply.github.com>
2026-01-24 16:57:46 +0100

    Revert "Removed caelestia-shell from home packages and autostart configuration; integrated caelestia settings for improved functionality."
    This reverts commit d9f435e2e4015cf43dea6399cc575cc8aaf9a7ed.

--- home/modules/quickshell/quickshell.nix
+++ home/modules/quickshell/quickshell.nix
@@ -3,6 +3,7 @@
 {
   home.packages = [
     inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default
+    inputs.caelestia-shell.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 
   # Link quickshell config to ~/.config/quickshell
@@ -16,26 +17,6 @@
   # Autostart quickshell
   wayland.windowManager.hyprland.settings.exec-once = [
     "quickshell"
+    "caelestia-shell"
   ];
-
-  programs.caelestia = {
-    enable = true;
-    systemd = {
-      enable = false; # if you prefer starting from your compositor
-      target = "graphical-session.target";
-      environment = [ ];
-    };
-    settings = {
-      bar.status = {
-        showBattery = true;
-      };
-      paths.wallpaperDir = "/etc/nixos/config/home/wallpapers";
-    };
-    cli = {
-      enable = true; # Also add caelestia-cli to path
-      settings = {
-        theme.enableGtk = false;
-      };
-    };
-  };
 }
