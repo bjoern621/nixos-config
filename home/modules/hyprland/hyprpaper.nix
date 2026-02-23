34e1ef8fbdf725f77fbbbc111d2a93499a28d5e3
Björn <41452212+bjoern621@users.noreply.github.com>
2026-02-23 01:41:43 +0100

    fixed wallpaper

--- home/modules/hyprland/hyprpaper.nix
+++ home/modules/hyprland/hyprpaper.nix
@@ -1,30 +1,22 @@
 { config, pkgs, ... }:
 
 let
-  #   wallpaper = "${config.home.homeDirectory}/.local/share/wallpapers/Zelda.no.Densetsu.full.3709265.jpg";
-  #   wallpaper = "/home/bjoern/.local/share/wallpapers/Zelda.no.Densetsu.full.3709265.jpg";
-  #   wallpaper = ../../wallpapers/Zelda.no.Densetsu.full.3709265.jpg;
+  wallpaper_rel_path = ".local/share/wallpapers";
 in
 {
-  #   # Copy the entire wallpapers directory.
-  home.file.".local/share/wallpapers".source = ../../wallpapers;
+  # Copy the entire wallpapers directory.
+  home.file."${wallpaper_rel_path}".source = ../../wallpapers;
 
-  #   # https://nix-community.github.io/home-manager/options.xhtml#opt-services.hyprpaper.enable
+  # https://nix-community.github.io/home-manager/options.xhtml#opt-services.hyprpaper.enable
   services.hyprpaper = {
     enable = true;
-    settings = {
-      preload = [ "/home/bjoern/.local/share/wallpapers/Ashes.jpg" ];
-      wallpaper = [ ",/home/bjoern/.local/share/wallpapers/Ashes.jpg" ];
-    };
+    settings.wallpaper = [
+      {
+        monitor = "";
+        path = "${config.home.homeDirectory}/${wallpaper_rel_path}/Honor.jpg";
+      }
+    ];
   };
-
-  #   # Ensure hyprpaper starts only after Hyprland has initialized its wayland
-  #   # socket and registered monitor outputs. Without this, graphical-session.target
-  #   # fires too early and hyprpaper finds no outputs, leaving a blank screen.
-  #   systemd.user.services.hyprpaper.Unit.After = [
-  #     "graphical-session.target"
-  #     "wayland-wm@hyprland.desktop.service"
-  #   ];
 
   # https://wiki.hypr.land/Configuring/Variables/#misc
   wayland.windowManager.hyprland.settings.misc = {
