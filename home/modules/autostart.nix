22501c300ffc6d3cf2f2c8e73b873d0efb759e44
Björn <41452212+bjoern621@users.noreply.github.com>
2026-03-07 00:27:44 +0100

    empty array

--- home/modules/autostart.nix
+++ home/modules/autostart.nix
@@ -3,8 +3,6 @@
 {
   wayland.windowManager.hyprland.settings = {
     exec = [
-      # Add autostart entries in their respective feature modules
-      # (e.g., nm-applet is in networkmanager.nix)
     ];
   };
 }
