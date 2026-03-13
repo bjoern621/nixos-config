8777f2ab1b5cf8a182e5a8460aae03029951c886
Björn <41452212+bjoern621@users.noreply.github.com>
2026-03-14 00:12:38 +0100

    moved claude config

--- home/modules/vscode.nix
+++ home/modules/vscode.nix
@@ -16,4 +16,18 @@
     ];
     type = "Application";
   };
+
+  # Needed by Claude Code Vscode Extension.
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc.lib
+    zlib
+    zlib.dev
+    fuse3
+    icu
+    nss
+    openssl
+    curl
+    expat
+  ];
 }
