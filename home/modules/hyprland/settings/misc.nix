3a1b2ded5b54ebfcb3760484a039c2e8c9f2e0b4
Björn <41452212+bjoern621@users.noreply.github.com>
2026-03-14 14:48:51 +0100

    comments

--- home/modules/hyprland/settings/misc.nix
+++ home/modules/hyprland/settings/misc.nix
@@ -3,7 +3,9 @@
 {
   # https://wiki.hypr.land/Configuring/Variables/#misc
   wayland.windowManager.hyprland.settings.misc = {
+    # Variable Frame Rate, reduces rendering when idle for power saving
     vfr = true;
+    # Variable Refresh Rate
     vrr = 1;
     disable_autoreload = true;
   };
