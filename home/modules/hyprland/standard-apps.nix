85714529e3338cf1863a995dd3ba38750fc6ff33
Björn <41452212+bjoern621@users.noreply.github.com>
2026-03-14 19:55:56 +0100

    launch code using desktop file

--- home/modules/hyprland/standard-apps.nix
+++ home/modules/hyprland/standard-apps.nix
@@ -1,6 +1,8 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  home.packages = [ pkgs.gtk3 ];
+
   wayland.windowManager.hyprland.settings.exec-once = [
     "[workspace 1 silent] google-chrome"
     "[workspace 2 silent] code --password-store=\"gnome-libsecret\" --touch-events"
