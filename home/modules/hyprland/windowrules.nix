149aa356f3d059d59966dab704d90eb96f04d5d1
Björn <41452212+bjoern621@users.noreply.github.com>
2026-03-21 23:06:08 +0100

    float bluetooth

--- home/modules/hyprland/windowrules.nix
+++ home/modules/hyprland/windowrules.nix
@@ -11,6 +11,7 @@
       "float on, match:class chrome-nngceckbapebfimnlniiiahkandclblb-Default"
       # Float common dialog windows (file pickers, save dialogs, etc.)
       "float on, match:title (Datei öffnen|Speichern unter|Ordner öffnen|Open File|Open Folder|Save As|Save File)"
+      "float on, match:class .blueman-manager-wrapped"
     ];
   };
 }
