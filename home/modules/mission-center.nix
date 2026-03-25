2560df4e9682bf27b5f138d34a2d8568a82def68
Björn <41452212+bjoern621@users.noreply.github.com>
2026-03-25 20:20:20 +0100

    Added performance settings for MissionCenter to enhance data handling and visualization.

--- home/modules/mission-center.nix
+++ home/modules/mission-center.nix
@@ -5,6 +5,9 @@
     "io/missioncenter/MissionCenter" = {
       performance-smooth-graphs = true;
       performance-sliding-graphs = true;
+      # 300 data points = 5 minutes (default 60 = 1 minute)
+      performance-page-data-points = 300;
+      apps-page-merged-process-stats = true;
     };
   };
 }
