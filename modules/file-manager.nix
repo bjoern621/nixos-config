3135ad59871152433dcf296e235bc27988ef7b5d
Björn <41452212+bjoern621@users.noreply.github.com>
2026-03-28 15:15:14 +0100

    Added nautilus-python and gvfs services for enhanced file management capabilities

--- modules/file-manager.nix
+++ modules/file-manager.nix
@@ -3,5 +3,10 @@
 {
   environment.systemPackages = with pkgs; [
     nautilus
+    nautilus-python # loads Python extensions from ~/.local/share/nautilus-python/extensions/
+    gvfs # sftp, smb, trash and network location backends
   ];
+
+  services.gvfs.enable = true;    # needed for trash and network browsing in Nautilus
+  services.udisks2.enable = true; # needed for removable media and mounting
 }
