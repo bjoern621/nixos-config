f7096687ce5d60d7cf124c63d5b36a8d539c0867
Björn <41452212+bjoern621@users.noreply.github.com>
2026-03-31 23:25:21 +0200

    Updated application launch commands in standard-apps.nix to use uwsm for improved compatibility with Hyprland.

--- home/modules/hyprland/standard-apps.nix
+++ home/modules/hyprland/standard-apps.nix
@@ -1,11 +1,8 @@
 { pkgs, ... }:
 
 {
-  # Gtk3 is needed for gtk-launch, which is used to run desktop files..
-  home.packages = [ pkgs.gtk3 ];
-
   wayland.windowManager.hyprland.settings.exec-once = [
-    "[workspace 1 silent] gtk-launch google-chrome"
-    "[workspace 2 silent] gtk-launch code"
+    "[workspace 1 silent] uwsm app -- google-chrome.desktop"
+    "[workspace 2 silent] uwsm app -- code.desktop"
   ];
 }
