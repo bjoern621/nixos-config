bbaeaa41e1a5c28b771878f63fa5884400bfb509
Björn <41452212+bjoern621@users.noreply.github.com>
2026-04-13 15:46:44 +0200

    Renamed k3s block to vmk3s in SSH configuration for clarity

--- home/modules/ssh-connect-homelab.nix
+++ home/modules/ssh-connect-homelab.nix
@@ -21,7 +21,7 @@
         identitiesOnly = true;
       };
 
-      k3s = {
+      vmk3s = {
         hostname = "192.168.178.80";
         user = "ops";
         identityFile = "/home/bjoern/.ssh/id_ed25519";
