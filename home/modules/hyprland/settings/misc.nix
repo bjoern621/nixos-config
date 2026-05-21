d66f443858911a30ea9412e56c8bcf0190fe153c
Björn <41452212+bjoern621@users.noreply.github.com>
2026-05-21 21:21:44 +0200

    chore: remove variable frame rate setting from Hyprland configuration

--- home/modules/hyprland/settings/misc.nix
+++ home/modules/hyprland/settings/misc.nix
@@ -3,8 +3,6 @@
 {
   # https://wiki.hypr.land/Configuring/Variables/#misc
   wayland.windowManager.hyprland.settings.misc = {
-    # Variable Frame Rate, reduces rendering when idle for power saving
-    vfr = true;
     # Variable Refresh Rate
     vrr = 1;
     # Disable Hyprland's automatic config reload on file changes, since we use sysconf-reload anyway
