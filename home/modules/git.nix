fc9604fc2da29545ad9278e7d8d8ce08d16fcebb
Björn <41452212+bjoern621@users.noreply.github.com>
2026-05-26 10:58:35 +0200

    fix: adjust user name formatting in git settings

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -13,10 +13,11 @@
 
     settings = {
       user = {
-        name  = "Björn";
+        name = "Björn";
         email = "41452212+bjoern621@users.noreply.github.com";
       };
       init.defaultBranch = "main";
+      pull.rebase = true;
 
       # Configure git-credential-helper with libsecret
       # Allows storing the git password and not needing to retype it over and over again
