e60d81e6431d23832f26e0fd1ed96009db6efecc
Björn <41452212+bjoern621@users.noreply.github.com>
2026-06-08 22:49:45 +0200

    fix: update smokeping configuration with additional probes and DNS settings

--- modules/smokeping.nix
+++ modules/smokeping.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
   services.smokeping = {
@@ -8,6 +8,18 @@
     # The nginx virtualHost serves the CGI at /smokeping.fcgi, not /smokeping.cgi.
     cgiUrl = "http://pi-4b-hh:8081/smokeping.fcgi";
 
+    probeConfig = ''
+      + FPing
+      binary = ${config.security.wrapperDir}/fping
+
+      + DNS
+      binary = ${pkgs.dig}/bin/dig
+      lookup = heise.de
+      pings = 5
+      step = 300
+      timeout = 3
+    '';
+
     targetConfig = ''
       probe = FPing
 
@@ -15,9 +27,37 @@
       title = Network Latency
       remark = Continuous latency and packet-loss probes
 
+      + Local
+      menu = Local
+      title = LAN Baseline (control)
+
+      ++ Router
+      menu = Fritz!Box (192.168.178.1)
+      title = Fritz!Box LAN side
+      host = 192.168.178.1
+
+      + Germany
+      menu = Germany
+      title = German Hosts
+
+      ++ Heise
+      menu = heise.de
+      title = heise.de
+      host = heise.de
+
+      ++ ARD
+      menu = ard.de
+      title = ARD (German public broadcaster)
+      host = ard.de
+
+      ++ CCC
+      menu = ccc.de
+      title = Chaos Computer Club
+      host = ccc.de
+
       + Internet
       menu = Internet
-      title = Internet
+      title = International Hosts
 
       ++ Cloudflare
       menu = Cloudflare (1.1.1.1)
@@ -34,10 +74,30 @@
       title = Quad9 DNS
       host = 9.9.9.9
 
-      ++ Heise
-      menu = heise.de
-      title = heise.de
-      host = heise.de
+      ++ GoogleDE
+      menu = google.de
+      title = Google Germany edge
+      host = google.de
+
+      ++ AmazonDE
+      menu = amazon.de
+      title = Amazon Germany
+      host = amazon.de
+
+      + DNS
+      menu = DNS Latency
+      title = DNS Query Times
+      probe = DNS
+
+      ++ CloudflareDNS
+      menu = Cloudflare (1.1.1.1)
+      title = DNS via Cloudflare
+      host = 1.1.1.1
+
+      ++ GoogleDNS
+      menu = Google (8.8.8.8)
+      title = DNS via Google
+      host = 8.8.8.8
     '';
   };
 
