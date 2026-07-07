98507aa75a961fa4e720cefffbb3dc1f12e5677f
Björn <41452212+bjoern621@users.noreply.github.com>
2026-07-07 23:47:19 +0200

    Removed element-desktop and mkchromecast from user packages

--- home/user-packages.nix
+++ home/user-packages.nix
@@ -5,13 +5,11 @@
     kdePackages.kate
     gimp
     obsidian
-    element-desktop
     kubectl
     virt-manager
     kubernetes-helm
     ripgrep
     bruno
-    mkchromecast
     claude-code
     dbeaver-bin
   ];
