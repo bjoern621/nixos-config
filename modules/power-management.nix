2c89fb4281ace24a61a9271330254a66fdb81925
Björn <41452212+bjoern621@users.noreply.github.com>
2026-07-21 01:32:07 +0200

    Updated power management settings for Radeon GPU to improve performance on battery.

--- modules/power-management.nix
+++ modules/power-management.nix
@@ -90,7 +90,7 @@
       RADEON_DPM_STATE_ON_AC = "performance";
       RADEON_DPM_STATE_ON_BAT = "battery";
       RADEON_DPM_PERF_LEVEL_ON_AC = "auto";
-      RADEON_DPM_PERF_LEVEL_ON_BAT = "low";
+      RADEON_DPM_PERF_LEVEL_ON_BAT = "auto";
     };
   };
 }
